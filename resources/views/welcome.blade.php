a49f15f6d0b8edd0db6b3c5b277c239bd98237dd
NikaKhiz <nika.khizambareli@redberry.ge>
2023-04-09 17:45:25 +0400

    Initialize coronatime app install necessary dependencies

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CoronaTime</title>
+
+    @vite('resources/css/app.css')
+
+</head>
+
+<body>
+    <div class="max-w-screen-md h-screen mx-auto flex items-center justify-center">
+        <h1 class="text-gray-900 text-3xl">CoronaTime</h1>
+    </div>
+</body>
+
+</html>
