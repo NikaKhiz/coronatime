6c703b1a7e79c88c929e06f47afcc44dfab25909
NikaKhiz <nika.khizambareli@redberry.ge>
2023-04-21 18:11:01 +0400

    refactor: create arrows component

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -16,55 +16,19 @@
                         <tr class="flex w-full">
                             <th scope="col" class="flex items-center gap-1 md:gap-2 w-1/4">
                                 location
-                                <div class="flex flex-col gap-1">
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 0.5L10 5.5L0 5.5L5 0.5Z" fill="#BFC0C4" />
-                                    </svg>
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 5.5L1.90735e-06 0.5H10L5 5.5Z" fill="#BFC0C4" />
-                                    </svg>
-                                </div>
+                                <x-statistics.arrows />
                             </th>
                             <th scope="col" class="flex items-center md:gap-2 w-1/4">
                                 new cases
-                                <div class="flex flex-col gap-1">
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 0.5L10 5.5L0 5.5L5 0.5Z" fill="#BFC0C4" />
-                                    </svg>
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 5.5L1.90735e-06 0.5H10L5 5.5Z" fill="#BFC0C4" />
-                                    </svg>
-                                </div>
+                                <x-statistics.arrows />
                             </th>
                             <th scope="col" class="flex items-center gap-1 md:gap-2 w-1/4">
                                 deaths
-                                <div class="flex flex-col gap-1">
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 0.5L10 5.5L0 5.5L5 0.5Z" fill="#BFC0C4" />
-                                    </svg>
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 5.5L1.90735e-06 0.5H10L5 5.5Z" fill="#BFC0C4" />
-                                    </svg>
-                                </div>
+                                <x-statistics.arrows />
                             </th>
                             <th scope="col" class="flex items-center gap-1 md:gap-2 w-1/4">
                                 recovered
-                                <div class="flex flex-col gap-1">
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 0.5L10 5.5L0 5.5L5 0.5Z" fill="#BFC0C4" />
-                                    </svg>
-                                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M5 5.5L1.90735e-06 0.5H10L5 5.5Z" fill="#BFC0C4" />
-                                    </svg>
-                                </div>
+                                <x-statistics.arrows />
                             </th>
                         </tr>
                     </thead>
