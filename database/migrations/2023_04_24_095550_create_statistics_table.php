4189e64ccc40ceefd827f07c241d0a3a39fbfbbe
NikaKhiz <nika.khizambareli@redberry.ge>
2023-04-24 14:00:59 +0400

    chore: create statistic model n migration

---
+++ database/migrations/2023_04_24_095550_create_statistics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('statistics', function (Blueprint $table) {
+            $table->id();
+            $table->string('country')->unique();
+            $table->string('code')->unique();
+            $table->integer('confirmed')->unsigned();
+            $table->integer('recovered')->unsigned();
+            $table->integer('critical')->unsigned();
+            $table->integer('deaths')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('statistics');
+    }
+};
