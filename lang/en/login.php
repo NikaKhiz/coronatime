ae753ff6e840c638409dfe5e34d241078d9f81ff
NikaKhiz <nika.khizambareli@redberry.ge>
2023-04-25 15:56:33 +0400

    feat: add translations to login page

---
+++ lang/en/login.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'welcome_back'    => 'Welcome Bacck',
+	'welcome_details' => 'Welcome Back! Please enter your details',
+	'remember_device' => 'Remember this device',
+	'forgot_pwd'      => 'Forgot password?',
+	'log_in'          => 'LOG IN',
+	'not_user'        => 'Don\'t have an accaunt?',
+	'register_link'   => 'Sign up for free',
+
+	'username'        => 'There is no user record with provided username or email.',
+];
