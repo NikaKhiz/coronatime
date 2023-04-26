78b4bb6797536856dd6f008c7135291886439aad
NikaKhiz <nika.khizambareli@redberry.ge>
2023-04-26 14:53:53 +0400

    refactor: add function return types

--- app/Http/Controllers/Statistic/StatisticController.php
+++ app/Http/Controllers/Statistic/StatisticController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Statistic;
 
 use App\Http\Controllers\Controller;
 use App\Models\Statistic;
+use Illuminate\View\View;
 
 class StatisticController extends Controller
 {
@@ -19,14 +20,14 @@ class StatisticController extends Controller
 		];
 	}
 
-	public function show()
+	public function show(): View
 	{
 		return view('dashboard', [
 			'worldwideStats' => $this->worldwideStats,
 		]);
 	}
 
-	public function index()
+	public function index(): View
 	{
 		return view('statistics', [
 			'statistics'     => Statistic::filter(request(['search', 'column', 'order']))->get(),
