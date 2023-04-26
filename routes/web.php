e9f7c0af9bbd9d0125c79a993cf6116afb1d8727
NikaKhiz <nika.khizambareli@redberry.ge>
2023-04-26 15:33:49 +0400

    refactor: group routes by controller

--- routes/web.php
+++ routes/web.php
@@ -17,28 +17,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/', 'login');
-Route::middleware('guest')->group(function () {
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+	Route::post('/login', 'login')->name('login_user');
+	Route::post('/register', 'register')->name('register_user');
+	Route::post('/forgot-password', 'recoverPassword')->name('recover_password');
+	Route::get('/reset-password/{token}', 'resetPasswordForm')->name('view.reset_password');
+	Route::post('/reset-password', 'resetPassword')->name('reset_password');
+	Route::get('/email/verify/{id}/{hash}', 'verifyEmail')->middleware('signed')->name('verification.verify');
+
 	Route::view('/login', 'auth.login')->name('view.login');
-	Route::post('/login', [AuthController::class, 'login'])->name('login_user');
 	Route::view('/register', 'auth.register')->name('view.register');
-	Route::post('/register', [AuthController::class, 'register'])->name('register_user');
-
 	Route::view('/email/verify', 'auth.verify-email')->name('verification.notice');
-
 	Route::view('/forgot-password', 'auth.forgot-password')->name('view.forgot_password');
-	Route::post('/forgot-password', [AuthController::class, 'recoverPassword'])->name('recover_password');
-
 	Route::view('/reset-password', 'auth.succes-pwd')->name('view.password_reset_success');
-	Route::get('/reset-password/{token}', [AuthController::class, 'resetPasswordForm'])->name('view.reset_password');
-	Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('reset_password');
 });
 
-Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->middleware('signed')->name('verification.verify');
-Route::Get('/language/{locale}', [LanguageController::class, 'changeLanguage'])->name('change_language');
-
 Route::middleware('auth')->group(function () {
-	Route::get('/dashboard', [StatisticController::class, 'show'])->name('dashboard');
-	Route::get('/statistics', [StatisticController::class, 'index'])->name('statistics');
+	Route::controller(StatisticController::class)->group(function () {
+		Route::get('/dashboard', 'show')->name('dashboard');
+		Route::get('/statistics', 'index')->name('statistics');
+	});
 
 	Route::get('/logout', [AuthController::class, 'logout'])->name('logout_user');
 });
+
+Route::Get('/language/{locale}', [LanguageController::class, 'changeLanguage'])->name('change_language');
