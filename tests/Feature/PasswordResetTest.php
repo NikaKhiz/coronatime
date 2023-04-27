8ba9616a86ae8bbc8e234af2ba43d6f6ac9f5a27
NikaKhiz <nika.khizambareli@redberry.ge>
2023-04-27 17:40:13 +0400

    test(password): password reset functionality

---
+++ tests/Feature/PasswordResetTest.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Password;
+use Tests\TestCase;
+
+class PasswordResetTest extends TestCase
+{
+	use RefreshDatabase;
+
+	public function test_password_submit_page_is_accessible()
+	{
+		$response = $this->get('/forgot-password');
+		$response->assertSuccessful();
+		$response->assertViewIs('auth.forgot-password');
+	}
+
+	public function test_password_submit_should_return_an_error_if_inputs_is_are_provided()
+	{
+		$response = $this->post('/forgot-password');
+		$response->assertSessionHasErrors('email');
+	}
+
+	public function test_password_submit_should_return_an_error_if_provided_email_is_invalid()
+	{
+		$email = 'johndoeexample.com';
+
+		$response = $this->post('/forgot-password', [
+			'email' => $email,
+		]);
+		$response->assertSessionHasErrors('email');
+	}
+
+	public function test_password_submit_should_return_an_error_if_there_is_no_user_record_with_provided_email()
+	{
+		$email = 'johndoe@example.com';
+
+		$response = $this->post('/forgot-password', [
+			'email' => $email,
+		]);
+		$response->assertSessionHasErrors('email');
+	}
+
+	public function test_password_submit_should_send_reset_link_to_user_registered_with_provided_email()
+	{
+		Notification::fake(ResetPassword::class);
+
+		$data = [
+			'email'    => 'johndoe@example.com',
+			'password' => bcrypt('password'),
+		];
+
+		$user = User::factory()->create($data);
+
+		$this
+		->followingRedirects()
+		->from('/fortot-password')
+		->post('/forgot-password', [
+			'email' => $user->email,
+		])
+		->assertSuccessful();
+
+		Notification::assertSentTo($user, ResetPassword::class);
+	}
+
+	public function test_password_reset_page_is_accessible()
+	{
+		$user = User::factory()->create();
+
+		$token = Password::createToken($user);
+
+		$this
+		->get('/reset-password/{token}', [
+			'token' => $token,
+		])
+		->assertViewIs('auth.reset-password')
+		->assertSuccessful();
+	}
+
+	public function test_password_reset_page_should_return_an_errors_if_no_inputs_provided()
+	{
+		$user = User::factory()->create([
+			'email'    => 'johndoe@example.com',
+		]);
+		$token = Password::createToken($user);
+
+		$response = $this->post('/reset-password', [
+			'email' => $user->email,
+			'token' => $token,
+		]);
+		$response->assertSessionHasErrors([
+			'password', 'password_confirmation',
+		]);
+	}
+
+	public function test_password_reset_page_should_return_an_errors_if_provided_password_is_less_than_three_characters()
+	{
+		$user = User::factory()->create([
+			'email'    => 'johndoe@example.com',
+		]);
+		$token = Password::createToken($user);
+
+		$response = $this->post('/reset-password', [
+			'email'   => $user->email,
+			'token'   => $token,
+			'password'=> 'ps',
+		]);
+		$response->assertSessionHasErrors([
+			'password', 'password_confirmation',
+		]);
+	}
+
+	public function test_password_reset_page_should_return_an_errors_if_password_confirmation_doesnt_match_to_password()
+	{
+		$user = User::factory()->create([
+			'email'    => 'johndoe@example.com',
+		]);
+		$token = Password::createToken($user);
+
+		$response = $this->post('/reset-password', [
+			'email'                 => $user->email,
+			'token'                 => $token,
+			'password'              => 'password',
+			'password_confirmation' => 'pasword',
+		]);
+		$response->assertSessionHasErrors([
+			'password', 'password_confirmation',
+		]);
+	}
+
+public function test_password_reset_page_should_update_password_for_current_user_if_he_provides_correct_information()
+{
+	Event::fake(PasswordReset::class);
+
+	$user = User::factory()->create([
+		'email'    => 'johndoe@example.com',
+	]);
+	$token = Password::createToken($user);
+
+	$response = $this->post('/reset-password', [
+		'email'                 => $user->email,
+		'token'                 => $token,
+		'password'              => 'password',
+		'password_confirmation' => 'password',
+	]);
+	$response->assertRedirect('/reset-password');
+
+	Event::assertDispatched(PasswordReset::class);
+}
+}
